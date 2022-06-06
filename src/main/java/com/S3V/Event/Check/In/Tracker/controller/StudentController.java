97dbabfd42b191dae838995c5e408782acb68223
SachetK <71898487+SachetK@users.noreply.github.com>
2022-06-06 10:32:55 -0400

    Update heroku

--- src/main/java/com/S3V/Event/Check/In/Tracker/controller/StudentController.java
+++ src/main/java/com/S3V/Event/Check/In/Tracker/controller/StudentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://event-check-in-tracker.herokuapp.com")
 @RestController
 @RequestMapping("/api/v1/")
 public class StudentController {
